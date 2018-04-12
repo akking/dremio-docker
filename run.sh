4bf2e6ab8266620d5a2488953f26b7e74dc31280
Dongyan Li <dli16@mail.depaul.edu>
2018-04-12 11:08:33 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,11 @@
 ## DREMIO Init
 echo "Starting Dremio ..."
 export JAVA_HOME=/usr/java/jre1.8.0_131
+
+sudo chown -R dremio:dremio /var/log/dremio
+sudo chown -R dremio:dremio /var/lib/dremio
+sudo chown -R dremio:dremio /var/run/dremio 
+
 /etc/init.d/dremio start
 
 echo "Watching Dremio Logs ..."
